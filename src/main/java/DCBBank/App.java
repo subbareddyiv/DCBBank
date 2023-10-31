098386011e862cda9caa7bf0ead42383852cfb51
subbareddyiv <subbareddyiv12@gmail.com>
2023-10-31 22:13:44 +0530

    Add files via upload

---
+++ src/main/java/DCBBank/App.java
@@ -0,0 +1,13 @@
+package DCBBank;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
